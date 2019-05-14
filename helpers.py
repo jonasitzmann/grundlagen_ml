ee26e02d90199b3fe99b6cc64d8cbf6efc9a81ad
Jonas Sitzmann <jonasitzmann@github.com>
2019-05-15 00:51:21 +0200

    finish ex4, there are still some bad formattings in the pdf

--- helpers.py
+++ helpers.py
@@ -1,8 +1,9 @@
+from __future__ import division
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from numpy.linalg import inv
-
+plt.style.use('fivethirtyeight')
 
 def load_dataset():
     return pd.read_csv('dataset.csv')
@@ -28,16 +29,24 @@ def plot_weights(weights, label, x_min, x_max, ax, **kwargs):
     ys = get_y(weights, xs)
     ax.plot(xs, ys, label=label, **kwargs)
 
-def plot(x, y=None, coeff_dict={}):
+def plot(x, y=None, coeff_dict={}, **kwargs):
+    n_items = len(coeff_dict.items())
     ax = plt.gca()
+    cm = plt.get_cmap('plasma')
     if not y is None:
-        ax.scatter(x, y, label='samples', marker='X', zorder=10)
+        ax.scatter(x, y, label='samples', marker='X', zorder=10, **kwargs)
     if not type(coeff_dict) == dict:
         coeff_dict = {'' : coeff_dict}
-    for label, w in coeff_dict.items():
-        plot_weights(w, label, min(x), max(x), ax)
+    for idx, (label, w) in enumerate(coeff_dict.items()):
+        plot_weights(w, label, min(x), max(x), ax, c=cm(idx/n_items), **kwargs)
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 def plot_df(df, coeff_dict={}):
     y = df.y if 'y' in df.columns else None
     plot(df.x, y, coeff_dict)
+
+def log_plot(*args, **kwargs):
+    fig, ax = plt.subplots()
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.plot(*args, **kwargs)
