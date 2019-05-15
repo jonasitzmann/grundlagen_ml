bf864c38cc47e33024f932a40ed8e25a1337d807
Jonas Sitzmann <jonasitzmann@github.com>
2019-05-15 22:54:23 +0200

    correct mistakes and formatting in ex4

--- helpers.py
+++ helpers.py
@@ -1,9 +1,11 @@
 from __future__ import division
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import pandas as pd
 from numpy.linalg import inv
 plt.style.use('fivethirtyeight')
+
 
 def load_dataset():
     return pd.read_csv('dataset.csv')
@@ -45,8 +47,30 @@ def plot_df(df, coeff_dict={}):
     y = df.y if 'y' in df.columns else None
     plot(df.x, y, coeff_dict)
 
+# ex4
+    
+def sample_mean(ys):
+    ys = np.array(ys)
+    mean = np.sum(ys, axis=0) / len(ys)
+    mean = mean.reshape(ys.shape[1:])
+    return mean
+
+def sample_variance(ys):
+    ys = np.array(ys)
+    return np.sum((ys - sample_mean(ys))**2) / (len(ys) - 1)
 def log_plot(*args, **kwargs):
     fig, ax = plt.subplots()
     ax.set_xscale('log')
     ax.set_yscale('log')
     ax.plot(*args, **kwargs)
+    
+def plot_samples(samples, distribution, label="Boccia Distribution"):
+    fig, ax = plt.subplots()
+    sns.distplot(samples, hist=False, rug=True, ax=ax, label=label)
+    draw_arrow(distribution.mu, 0.8, "true mean", ax, color='r')
+    draw_arrow(sample_mean(samples), 1.1, "sample mean", ax, color='b')
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5));
+
+def draw_arrow(x_pos, size, label, ax, **kwargs):
+    plt.arrow(x_pos, size + 0.5, 0, -size, axes=ax, width=0.005, head_length=0.3, **kwargs)
+    plt.text(x_pos, size + 0.6, label, axes=ax, horizontalalignment='center', **kwargs)
